d2ac254cee648c424ae24b9abe21b3aad0d0620a
Harsh-Vaishnani <harshvaishnani8@gmail.com>
2024-10-11 11:49:03 +0530

    C-Language

---
+++ CommonPractice/practice.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main(){
+
+//QUE-1   123456789 print krava mate
+    // int num[3][3]={{1,2,3},
+    //                 {4,5,6},
+    //                 {7,8,9}};
+    // for(int i=0; i<=2; i++){
+    //     for(int j=0; j<=2; j++){
+    //         printf("%d ",num[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+
+//QUE-2   odd and even sum in 10 number
+    // int num=10;
+    // int oddsum=0;
+    // int evensum=0;
+    // for(int i=1; i<=num; i++){
+    //     if(i%2==0){
+    //         evensum=evensum+i;
+    //     }
+    //     else{
+    //         oddsum=oddsum+i;
+    //     }
+    // }
+    // printf("odd sum %d ",oddsum);
+    // printf("even sum %d",evensum);
+
+
+// write a program to print the average of 3 numbers.
+    printf("%d\n",);
+}
---
+++ function/function.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+// * * jyare ky pn return krvanu NA hoy tyare * *
+// void second(){
+//     printf("harsh\n");
+//     printf("how are you\n");
+//     return;
+// }
+// int main(){
+//     second(); 
+//     second();
+//     second();
+//     return 0;
+// }
+
+
+
+// * * jyare kyk return krvanu HOY tyare * *
+// int add(int a, int b){
+//     return a+b;
+// }
+
+// int main(){
+//     int a;
+//     printf("Enter the first value :- ");
+//     scanf("%d",&a);
+//     int b;
+//     printf("Enter the second value :- ");
+//     scanf("%d",&b);
+    
+//     int sum=add(a,b);
+//     printf("%d",sum);
+//     return 0;
+// }
+
+
+
+// * * jyare koy no square root joto hoy tyare * *
+// #include <stdio.h>
+// #include <math.h>
+// int main(){
+//     float a;
+//     printf("Enter the value :- ");
+//     scanf("%f",&a);
+
+//     float root=sqrt(a);
+//     printf("%.2f",root);
+
+//     return 0;
+// }
+
+
+// * * jyare koy no power of number(ghat) joto hoy tyare * *
+#include <stdio.h>
+#include <math.h>
+int main(){
+    int h=pow(5,9);
+    printf("%d",h);
+    return 0;
+}
